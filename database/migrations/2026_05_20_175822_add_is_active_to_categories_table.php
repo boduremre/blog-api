de338fd9345331e72c5adc5f31f0c093bf3dc933
Emre Bodur <emrebodurbto@gmail.com>
2026-05-20 20:59:49 +0300

    Create 2026_05_20_175822_add_is_active_to_categories_table.php

---
+++ database/migrations/2026_05_20_175822_add_is_active_to_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            // is_active sütunu ekle
+            $table->boolean('is_active')->default(true)->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            // is_active sütununu kaldır
+            $table->dropColumn('is_active');
+        });
+    }
+};
