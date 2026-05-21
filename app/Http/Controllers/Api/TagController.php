5276bcbb1f41783a2cae049c8aa821e68e44dfec
boduremre <emrebodurbto@gmail.com>
2026-05-21 16:46:32 +0300

    Create TagController.php

---
+++ app/Http/Controllers/Api/TagController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Tag::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tag $tag)
+    {
+        return response()->json($tag);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tag $tag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tag $tag)
+    {
+        return response()->json([
+            'success' => $tag->delete(),
+            'message' => $tag->delete() ? 'Tag deleted successfully!' : 'Failed to delete tag.'
+        ]);
+    }
+}
