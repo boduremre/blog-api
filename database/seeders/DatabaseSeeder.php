e62c592e269145cd5dae828bbadf2ba0473ed630
boduremre <emrebodurbto@gmail.com>
2026-05-21 16:47:17 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,7 +20,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         //User::factory(10)->create();
+        //Category::factory(10)->create();
+        //Post::factory(50)->->hasAttached(Tag::factory()->count(30))->create();
+        //Comment::factory(100)->create();
 
+        
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
