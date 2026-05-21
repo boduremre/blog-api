533cd9ed925597371a2711bea44f85eb20c77ad8
boduremre <emrebodurbto@gmail.com>
2026-05-21 16:46:37 +0300

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Database\Factories\CommentFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    /** @use HasFactory<CommentFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
         'content',
         'post_id',
