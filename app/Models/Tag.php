7ece9e9b9f5dc68ed7b3efafc9216c6db949fc1c
boduremre <emrebodurbto@gmail.com>
2026-05-21 16:46:43 +0300

    Create Tag.php

---
+++ app/Models/Tag.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\TagFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends Model
+{
+    /** @use HasFactory<TagFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'is_active',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Post::class
+        );
+    }
+}
