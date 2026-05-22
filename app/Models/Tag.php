ca28ede3b9cc639770932a9b5669496e45e79aae
Emre Bodur <emrebodurbto@gmail.com>
2026-05-22 21:35:02 +0300

    Update Tag.php

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -34,8 +34,6 @@ class Tag extends Model
 
     public function posts(): BelongsToMany
     {
-        return $this->belongsToMany(
-            Post::class
-        );
+        return $this->belongsToMany(Post::class);
     }
 }
